ff8af3c2ebda47d7f8ec2f50e78d24b418be29a9
Bartal David <bartaldvd@gmail.com>
2023-01-09 15:58:47 +0100

    show route icons on saved stops

--- src/components/Stop.svelte
+++ src/components/Stop.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import type { components } from "../data/bkk-openapi";
-  import { savedStops } from "../data/stores";
+  import { savedStops, savedRouteRef } from "../data/stores";
 
   import { MdFavoriteBorder, MdFavorite } from "svelte-icons/md";
+  import { prevent_default } from "svelte/internal";
   export let references: components["schemas"]["TransitReferences"] = {};
   export let stop: components["schemas"]["TransitStop"] = {};
   $: saved = $savedStops.some((savedStop) => savedStop.id == stop.id);
@@ -11,6 +12,15 @@
   function toggleStop(stop: components["schemas"]["TransitStop"]) {
     if (!saved) {
       savedStops.update((prev) => [...prev, stop]);
+      let routeRefForStop: {
+        [key: string]: components["schemas"]["TransitRoute"] | undefined;
+      } = {};
+      stop.routeIds.forEach((routeId) => {
+        routeRefForStop[routeId] = references.routes[routeId];
+      });
+      savedRouteRef.update((prev) => {
+        return { ...prev, ...routeRefForStop };
+      });
     } else {
       savedStops.update((prev) => prev.filter((e) => e.id !== stop.id));
     }
@@ -21,8 +31,8 @@
   <div class="flex-1">
     <div class="mb-1">{stop.name}</div>
     <div class="flex flex-row flex-wrap gap-1">
+      <!-- TODO it would be nicer if savedrouteref would be passed down as references.routes here -->
       {#if references.routes}
-        <!-- FIXME this should be working if the references are saved to a store -->
         {#each stop?.routeIds || [] as routeid}
           <span
             class="rounded p-1 text-xs"
@@ -31,6 +41,17 @@
               references.routes[routeid].style.color}
             >{references.routes[routeid].shortName}
           </span>
+        {/each}
+      {:else}
+        {#each stop?.routeIds || [] as routeid}
+          {#if $savedRouteRef[routeid]}
+            <span
+              class="rounded p-1 text-xs"
+              style:color={"#" + $savedRouteRef[routeid].style.icon.textColor}
+              style:background-color={"#" + $savedRouteRef[routeid].style.color}
+              >{$savedRouteRef?.[routeid]?.shortName || ""}
+            </span>
+          {/if}
         {/each}
       {/if}
 
--- src/data/stores.ts
+++ src/data/stores.ts
@@ -2,15 +2,25 @@ import { writable } from "svelte/store";
 import type { components } from "./bkk-openapi";
 import defaultStops from "./defaultStops";
 
-const LOCAL_STORAGE_KEY = "bkk.stops";
+const STOPS_LSKEY = "bkk.stops";
+const ROUTE_REF_LSKEY = "bkk.reference";
 
-const stored = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+const storedStops = JSON.parse(localStorage.getItem(STOPS_LSKEY));
 export const savedStops = writable<components["schemas"]["TransitStop"][]>(
-  stored || defaultStops
+  storedStops || defaultStops
 );
 
 savedStops.subscribe((value) =>
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(value))
+  localStorage.setItem(STOPS_LSKEY, JSON.stringify(value))
+);
+
+const storedRouteRef = JSON.parse(localStorage.getItem(ROUTE_REF_LSKEY));
+export const savedRouteRef = writable<{
+  [key: string]: components["schemas"]["TransitRoute"] | undefined;
+}>(storedRouteRef);
+
+savedRouteRef.subscribe((value) =>
+  localStorage.setItem(ROUTE_REF_LSKEY, JSON.stringify(value))
 );
 
 export const editMode = writable(false);
