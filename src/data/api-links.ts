28c0594b978277a5d87ada9771c161b6a1e1c3ff
Bartal David <bartaldvd@gmail.com>
2023-04-13 21:51:08 +0200

    api keys

--- src/data/api-links.ts
+++ src/data/api-links.ts
@@ -1,6 +1,5 @@
-export const stopsForLocationUrl =
-  "https://futar.bkk.hu/api/query/v1/ws/otp/api/where/stops-for-location.json?";
-export const stopDataUrl =
-  "https://futar.bkk.hu/api/query/v1/ws/otp/api/where/arrivals-and-departures-for-stop.json?";
-export const tripDataUrl =
-  "https://futar.bkk.hu/api/query/v1/ws/otp/api/where/trip-details.json?";
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const stopsForLocationUrl = `https://futar.bkk.hu/api/query/v1/ws/otp/api/where/stops-for-location.json?key=${API_KEY}&`;
+export const stopDataUrl = `https://futar.bkk.hu/api/query/v1/ws/otp/api/where/arrivals-and-departures-for-stop.json?key=${API_KEY}&`;
+export const tripDataUrl = `https://futar.bkk.hu/api/query/v1/ws/otp/api/where/trip-details.json?key=${API_KEY}&`;
